9b7f2425afe6f937d2485697522c7dc1ec3f7674
Manuel Sebastian Blanco <msb@Manuels-MacBook-Pro.local>
2021-01-23 20:50:41 -0300

    Se agrega modulo pageNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projets-component/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path: 'Projects`',
+    component: ProjectsComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/projets-component/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projets-component',
+  templateUrl: './projects-component.component.html',
+  styleUrls: ['./projects-component.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

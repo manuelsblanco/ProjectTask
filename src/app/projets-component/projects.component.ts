c60a468a725bb3b5c6963943ed8d006925edfc48
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-01-26 21:57:14 -0300

    Boton de navigate to home

--- src/app/projets-component/projects.component.ts
+++ src/app/projets-component/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-projets-component',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
+
+  naviHome(){
+    this.route.navigate(['']);
+  }
 
   ngOnInit(): void {
   }
